bd725d7ee0238023dfd3b4ee4ea0f15e786946d1
Ru-bot347 <russelnickpayod@gmail.com>
2024-12-11 09:27:55 +0800

    first comit

---
+++ controllers/booksController.js
@@ -0,0 +1,21 @@
+const books = require("../models/booksModel");
+const getAllbooks = (req, res) => {
+    res.json(books);
+};
+
+const getBooksByGenre = (req, res) => {
+    const genre = req.params.genre;
+    const filteredBooks = books.filter((book) => book.genre === genre);
+
+    if (filteredBooks.length === 0) {
+        return res.status(404).json({ error: "No genre found in books" });
+    }
+    res.json(filteredBooks);
+
+};
+
+module.exports = {
+    getAllbooks,
+    getBooksByGenre,
+};
+
